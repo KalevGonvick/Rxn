d94f3f612a4913fb5ed3dd25e6766b24e6a2fe96
Kalev Gonvick <kalev.gonvick@outlook.com>
2023-07-16 16:43:51 -0700

    Added shape base class + moved some example code to reusable classes - WORKING STATE

---
+++ RxnEngine/Source/Platform/WIN32/Caption.cpp
@@ -0,0 +1,84 @@
+#include "Rxn.h"
+#include "Caption.h"
+
+namespace Rxn::Platform::Win32
+{
+    Caption::Caption()
+        : m_ShowTitle(1)
+    {
+    }
+
+    Caption::~Caption() = default;
+
+
+    void Caption::AddButton(const WString &text, const Command &command, int32 width)
+    {
+        int32 newButtonOffset = 0;
+
+        for (auto &existingBtn : m_Buttons)
+        {
+            newButtonOffset += existingBtn->width;
+        }
+
+        int32 newId = m_Buttons.size() + 1;
+        std::shared_ptr<Caption::Button> newButton = std::make_shared<Caption::Button>(newId, text, command, newButtonOffset, width);
+        m_Buttons.push_back(newButton);
+    }
+
+    void Caption::RemoveButton(const int32 &removeId)
+    {
+        for (auto it = m_Buttons.begin(); it != m_Buttons.end();)
+        {
+            if (it->get()->id == removeId)
+            {
+                m_Buttons.erase(it);
+                break;
+            }
+            else
+            {
+                it++;
+            }
+        }
+
+
+    }
+
+    std::list<std::shared_ptr<Caption::Button>> &Caption::GetButtons()
+    {
+        return m_Buttons;
+    }
+
+    Command Caption::GetWindowCaptionButtonClicked(POINT &cursor, const RECT &windowRect)
+    {
+        cursor.x -= windowRect.left;
+        cursor.y -= windowRect.top;
+
+        RXN_LOGGER::PrintLnSeperator();
+        RXN_LOGGER::Trace(L"Cursor at x: %d y: %d", cursor.x, cursor.y);
+        RXN_LOGGER::Trace(L"Window at top: %d, left: %d, right: %d, bottom: %d", windowRect.top, windowRect.left, windowRect.right, windowRect.bottom);
+
+        for (auto currentButton = m_Buttons.begin(); currentButton != m_Buttons.end(); currentButton++)
+        {
+            std::shared_ptr<Button> itButton = *currentButton;
+            RXN_LOGGER::Trace(L"Button rect at top: %d, left: %d, right: %d, bottom: %d", itButton->rect.top, itButton->rect.left, itButton->rect.right, itButton->rect.bottom);
+
+
+            if (itButton->rect.left < cursor.x && itButton->rect.right > cursor.x && itButton->rect.top < cursor.y && itButton->rect.bottom > cursor.y)
+            {
+                RXN_LOGGER::Trace(L"Click within bounds!!!");
+                RXN_LOGGER::PrintLnSeperator();
+                return itButton->cmd;
+            }
+        }
+        RXN_LOGGER::PrintLnSeperator();
+
+
+        return Command::CB_NOP;
+    }
+
+
+
+
+
+
+}
---
+++ RxnEngine/Source/Platform/WIN32/WindowManager.cpp
@@ -0,0 +1,54 @@
+#include "Rxn.h"
+#include "WindowManager.h"
+
+
+namespace Rxn::Platform::Win32
+{
+
+
+    WindowManager::WindowManager()
+        : m_ManagedWindows()
+        , m_WindowCounter(0)
+    {
+    }
+
+    WindowManager::~WindowManager() = default;
+
+
+    void WindowManager::CreateNewWindow(WindowDesc &windowDesc)
+    {
+
+        if (m_ManagedWindows.find(windowDesc.windowClassName) != m_ManagedWindows.end())
+        {
+            RXN_LOGGER::Error(L"Window class %s already exists.");
+            return;
+        }
+
+        auto window = std::make_shared<Window>(windowDesc.windowName, windowDesc.windowClassName);
+
+        window->m_WindowBackgroundColour = windowDesc.windowBgColour;
+        window->m_WindowBorderColour = windowDesc.windowBorderColour;
+        window->m_WindowActiveBorderHighlightColour = windowDesc.windowActiveBorderHighlightColour;
+        window->m_WindowStyle = windowDesc.windowStyle;
+
+        m_ManagedWindows.emplace(window->GetClass(), window);
+        m_WindowCounter++;
+    }
+
+
+    void WindowManager::AddWindow(std::shared_ptr<Window> &window)
+    {
+        if (m_ManagedWindows.find(window->GetClass()) != m_ManagedWindows.end())
+        {
+            RXN_LOGGER::Error(L"Window class %s already exists.");
+            return;
+        }
+
+        RXN_LOGGER::Info(L"Adding Window %s", window->GetClass().c_str());
+
+        m_ManagedWindows.emplace(window->GetClass(), window);
+        m_WindowCounter++;
+    }
+}
+
+
